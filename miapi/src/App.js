6041de274d157e18131127117a202909fcd987a5
Juan Cruz Gonzalez <juan.cruz_gonzalez@hotmail.com>
2023-02-03 13:33:51 +0100

    creado variables para conectar la API

--- miapi/src/App.js
+++ miapi/src/App.js
@@ -1,9 +1,22 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [valor, setValor] = useState('')
+
+  const buscarResultado = async () =>{
+     const apiKey = 'fb1CvmzqIIYRl2gsxMbOhk3x9JKFg6HDz_Qr8NZKK8Q'
+     const URL = `https://api.unsplash.com/photos/?client_id=${apiKey}`;
+
+     const response = await fetch(URL);
+     const data = await response.json();
+     console.log(data)
+  }
   return (
     <div className="App">
-     
+       <input className='box-imput' placeholder='Buscar Imagenes' onChange={e=> setValor(e.target.value)}/>
+
+       <button onClick={() => buscarResultado ()} className='btn' type=""> Buscar</button>
     </div>
   );
 }
