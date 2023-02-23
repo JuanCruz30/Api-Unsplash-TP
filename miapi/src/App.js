b90d71fdf84f59ea4a42f4134a9fd7938e66bbd7
Juan Cruz Gonzalez <juan.cruz_gonzalez@hotmail.com>
2023-02-23 16:51:30 -0300

    descargando chakra ui

--- miapi/src/App.js
+++ miapi/src/App.js
@@ -1,44 +1,67 @@
-import { useState } from 'react';
+import { useState} from 'react';
 import './App.css';
-import { GrSearch } from "react-icons/gr";
+
+import InfiniteScroll from 'react-infinite-scroll-component';
+
 
 function App() {
   const [valor, setValor] = useState('')
-  
+
   const [resultados, setResultados] = useState([])
 
-  const buscarResultado = async () =>{
-     const apiKey = 'fb1CvmzqIIYRl2gsxMbOhk3x9JKFg6HDz_Qr8NZKK8Q'
-     const URL = `https://api.unsplash.com/search/photos/?client_id=${apiKey}&query=${valor}&per_page=50`;
 
-     const response = await fetch(URL);
-     const data = await response.json();
-     setResultados  (data.results)
-     console.log(data);
+
+  const buscarResultado = async () => {
+    const apiKey = 'fb1CvmzqIIYRl2gsxMbOhk3x9JKFg6HDz_Qr8NZKK8Q'
+    const URL = `https://api.unsplash.com/search/photos?client_id=${apiKey}&query=${valor}&per_page=50`;
+
+    const response = await fetch(URL);
+    const data = await response.json();
+    setResultados(data.results)
+    console.log(data);
   }
   return (
     <div className='contenedorprin'>
 
-      <div className="app">
 
-        <input className='box-imput' placeholder='Buscar Imagenes' onChange={e => setValor(e.target.value)} />
 
-        <button onClick={() => buscarResultado()} className='btn' type=""><GrSearch className='lupa'/></button>
-      </div>
 
       <div className='contenedordeimagenes' >
-        {
-          resultados.map((elemento, indice) => {
-            return (
-              
-                <img key={indice} src={elemento.urls.regular} />
-              
 
-            )
-          })
-        }
-      </div>
-    
+        <InfiniteScroll
+          dataLength={resultados.length}
+          next={buscarResultado}
+          hasMore={true}
+        >
+          <div className="app">
+
+            <input className='box-imput' placeholder='Buscar Imagenes' onChange={e => setValor(e.target.value)} />
+
+            <button onClick={() => buscarResultado()} className='btn' type=""></button>
+          </div>
+
+          {
+            resultados.map((elemento, indice) => {
+              return (
+                <div key={indice}>
+                  <img src={elemento.urls.regular} />
+                  <h1>{elemento.id}</h1>
+                  <h2 >{elemento.user.location}</h2>
+
+                </div>
+
+
+
+
+              )
+
+            })
+
+          }
+        </InfiniteScroll>
+      </div >
+
+
     </div>
   );
 }
