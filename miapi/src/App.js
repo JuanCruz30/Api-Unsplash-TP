286e45a2f8c833c8b6f1e1c7538961ee57da34af
Juan Cruz Gonzalez <juan.cruz_gonzalez@hotmail.com>
2023-02-25 14:55:46 -0300

    usando FLexBox con chakra

--- miapi/src/App.js
+++ miapi/src/App.js
@@ -1,7 +1,13 @@
-import { useState} from 'react';
+import {  useEffect, useState} from 'react';
 import './App.css';
 
-import InfiniteScroll from 'react-infinite-scroll-component';
+
+
+import { Buttonn } from './Components/main';
+import { Box, Heading, Text, Input,Image, Button,Stack,Card,CardBody,CardFooter} from '@chakra-ui/react';
+
+
+
 
 
 function App() {
@@ -9,60 +15,80 @@ function App() {
 
   const [resultados, setResultados] = useState([])
 
+ 
 
 
+
+  
+  
   const buscarResultado = async () => {
     const apiKey = 'fb1CvmzqIIYRl2gsxMbOhk3x9JKFg6HDz_Qr8NZKK8Q'
-    const URL = `https://api.unsplash.com/search/photos?client_id=${apiKey}&query=${valor}&per_page=50`;
-
+    const URL = `https://api.unsplash.com/search/photos?client_id=${apiKey}&query=${valor}&page=10&per_page=50`;
+    
     const response = await fetch(URL);
     const data = await response.json();
     setResultados(data.results)
     console.log(data);
-  }
+    
+    
+  };
+  
+  
+    useEffect(()=>{
+      buscarResultado();
+  },[]);
+  
+
+ 
+  
   return (
-    <div className='contenedorprin'>
+    <Box gap='2rem'  display='flex' flexWrap='wrap' margin='auto' alignItems='center' flexDirection='column' justifyContent='center'>
+    
+        <Box display='flex'>
+        <Input width='200px' placeholder='Buscar Imagenes' onChange={e => setValor(e.target.value)} />
 
-
-
-
-      <div className='contenedordeimagenes' >
-
-        <InfiniteScroll
-          dataLength={resultados.length}
-          next={buscarResultado}
-          hasMore={true}
-        >
-          <div className="app">
-
-            <input className='box-imput' placeholder='Buscar Imagenes' onChange={e => setValor(e.target.value)} />
-
-            <button onClick={() => buscarResultado()} className='btn' type=""></button>
-          </div>
-
-          {
-            resultados.map((elemento, indice) => {
+         <Button onClick={() => buscarResultado() } >click</Button>
+         
+        
+        </Box>
+        <Box  gap='1rem' display='flex' flexWrap='wrap' width='1200px' >
+          
+          
+           {resultados.map((elemento, indice) => {
               return (
-                <div key={indice}>
-                  <img src={elemento.urls.regular} />
-                  <h1>{elemento.id}</h1>
-                  <h2 >{elemento.user.location}</h2>
+                <Box
+                
+                 
+                 flexWrap='wrap'
+                 boxShadow='dark-lg'
+                 borderRadius='20px'
+                 display='flex'
+                 justifyContent='center' 
+                 textAlign='center' 
+                 flexDirection='column'  
+                 width='380px' 
+                 height='450px' 
+                 key={indice}>
+                  <Box margin='0 auto' width='300px'>
+                    <Image boxShadow='2xl' width='100%' height='300px' objectFit='cover'  src={elemento.urls.regular}/>
+                  </Box>
+                  <Text>{elemento.id}</Text>
+                  <Text>Ubicacion:{elemento.id}</Text>
 
-                </div>
+                  </Box>
 
 
-
-
-              )
-
-            })
+              
+                
+                ) })
 
           }
-        </InfiniteScroll>
-      </div >
+          
+        
+        
+          </Box>
 
-
-    </div>
+    </Box>
   );
 }
 
---
+++ miapi/src/Components/Button/Buttonn.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Button } from '@chakra-ui/react'
+
+
+const Buttonn = ( {buscarResultado}) => {
+  return (
+    <Button onClick={buscarResultado}>hola</Button>
+  )
+}
+
+export {Buttonn}
